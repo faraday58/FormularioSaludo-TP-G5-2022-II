bd54a218db8b411ff659f07da0e270c55a77a5b0
Armando Rodríguez <jarodriguezvera@gmail.com>
2022-02-21 12:51:52 -0600

    Agregando botón

--- FormSaludo.Design.cs
+++ FormSaludo.Design.cs
@@ -7,6 +7,8 @@ namespace FormularioSaludo_TP_G5_2022_II
     partial class FormSaludo:Form
     {
         private Label lbMensaje;
+        private TextBox txtbNombre;
+        private Button btnSaludo;
         private void InicializarComponentes()
         {
             this.Text = "FormSaludo";
@@ -17,7 +19,18 @@ namespace FormularioSaludo_TP_G5_2022_II
             lbMensaje.Location = new Point(20, 50);
             lbMensaje.Size = new Size(300,15);
 
+            txtbNombre = new TextBox();
+            txtbNombre.Location = new Point(100,150);
+            txtbNombre.Size = new Size(50,50);
+
+            btnSaludo = new Button();
+            btnSaludo.Text = "Aceptar";
+            btnSaludo.Location = new Point(100, 200);
+            btnSaludo.Size = new Size(100, 50);
+
             this.Controls.Add(lbMensaje);
+            this.Controls.Add(txtbNombre);
+            this.Controls.Add(btnSaludo);
 
         }
 
