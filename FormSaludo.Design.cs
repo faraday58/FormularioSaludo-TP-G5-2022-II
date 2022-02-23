b5d2131f4ee02a5cbb4ca597f3842fb76747e401
Armando Rodríguez <jarodriguezvera@gmail.com>
2022-02-23 12:51:39 -0600

    Ejemplo ya con el saludo

--- FormSaludo.Design.cs
+++ FormSaludo.Design.cs
@@ -10,10 +10,12 @@ namespace FormularioSaludo_TP_G5_2022_II
         private TextBox txtbNombre;
         private Button btnSaludo;
         private void InicializarComponentes()
-        {
+        {           
+            //Atributos y propiedades de la Clase FormSaludos
+           
             this.Text = "FormSaludo";
             this.Size = new Size(400, 300);
-
+           
             lbMensaje = new Label();
             lbMensaje.Text = "Introduce tu nombre";
             lbMensaje.Location = new Point(20, 50);
@@ -21,17 +23,20 @@ namespace FormularioSaludo_TP_G5_2022_II
 
             txtbNombre = new TextBox();
             txtbNombre.Location = new Point(100,150);
-            txtbNombre.Size = new Size(50,50);
+            txtbNombre.Size = new Size(150,50);
+
 
             btnSaludo = new Button();
             btnSaludo.Text = "Aceptar";
             btnSaludo.Location = new Point(100, 200);
             btnSaludo.Size = new Size(100, 50);
 
+            btnSaludo.Click += new EventHandler(Click_Saludo);
+
             this.Controls.Add(lbMensaje);
             this.Controls.Add(txtbNombre);
             this.Controls.Add(btnSaludo);
-
+           
         }
 
     }
--- FormSaludo.cs
+++ FormSaludo.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Windows.Forms;
 
 namespace FormularioSaludo_TP_G5_2022_II
 {
     partial class FormSaludo
     {
+        //Métodos de la Clase FormSualudo
         public FormSaludo()
         {
             InicializarComponentes();
+        }
+
+        public void Click_Saludo(object sender, EventArgs e )
+        {
+            MessageBox.Show("Hola " + txtbNombre.Text);
         }
     }
 }
